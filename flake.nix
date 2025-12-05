a21b737a2dc049e8693a60ccb6628cf2c88dbcb9
inferno214221 <inferno214221@gmail.com>
2025-12-06 10:03:42 +1030

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "Invoice Generator";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+    
+    fenix = {
+      url = "github:nix-community/fenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    naersk-pkg = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, fenix, naersk-pkg, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ fenix.overlays.default ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+        toolchain = fenix.packages.${system}.fromToolchainFile {
+          file = ./rust-toolchain.toml;
+          sha256 = "sha256-P39FCgpfDT04989+ZTNEdM/k/AE869JKSB4qjatYTSs=";
+        };
+        naersk = pkgs.callPackage naersk-pkg {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
+        buildInputs = with pkgs; [];
+        nativeBuildInputs = with pkgs; [
+          toolchain
+          pkg-config
+          gcc
+          cargo-expand
+          cargo-public-api
+          rust-analyzer-nightly
+          openssl
+        ] ++ buildInputs;
+      in with pkgs; rec
+      {
+        devShells.default = mkShell {
+          inherit nativeBuildInputs;
+
+          LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath [
+            pkgs.openssl
+          ];
+        };
+      }
+    );
+}
